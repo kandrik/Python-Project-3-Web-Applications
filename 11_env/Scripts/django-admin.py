095897de479fb2a3fbe73007d00aa2c41e04124e
Chad <mchuth@gmail.com>
2019-03-31 15:51:41 -0500

    Initial creation of learning_logs app

---
+++ 11_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\chad\documents\software projects\python-project-3-web-applications\11_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
